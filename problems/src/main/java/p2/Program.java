2ae6e057a87a010d2c7fed29f5c94ded75fcfb05
ghchoi <ghchoi0427@gmail.com>
2022-09-01 10:17:15 +0900

    fix: 프로젝트 구조 변경

---
+++ problems/src/main/java/p2/Program.java
@@ -0,0 +1,12 @@
+package p2;
+
+public class Program {
+    public static void main(String[] args) {
+        for (int i = 1; i < 101; i++) {
+            if (i % 7 == 0 && i % 8 == 0) {
+                System.out.printf("찾는 정수 : %d",i);
+                break;
+            }
+        }
+    }
+}
